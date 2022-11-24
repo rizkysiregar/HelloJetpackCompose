9522b4d310719a85342d6cd0e2c2a66f4135e28e
Habib Rizky Almajid Siregar <regarrezky@gmail.com>
2022-11-24 10:12:18 +0700

    Initial commit

---
+++ app/src/main/java/com/rizkysiregar/hellojetpackcompose/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.rizkysiregar.hellojetpackcompose
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rizkysiregar.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+
+
+private val sampleName = listOf(
+    "Andre",
+    "Desta",
+    "Parto",
+    "Wendy",
+    "Komeng",
+    "Raffi Ahmad",
+    "Andhika Pratama",
+    "Vincent Ryan Rompies"
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloJetpackComposeTheme {
+                HelloJetpackComposeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloJetpackComposeApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    var isExpended by remember { mutableStateOf(false)}
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpended) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+   Card(
+       backgroundColor = MaterialTheme.colors.primary,
+       shape = MaterialTheme.shapes.medium,
+       modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+   ) {
+       Row(
+           modifier = Modifier.padding(8.dp),
+           verticalAlignment = Alignment.CenterVertically
+       ) {
+           Image(
+               painter = painterResource(R.drawable.jetpack_compose),
+               contentDescription = "Logo Jetpack Compose",
+               modifier = Modifier.size(animatedSizeDp)
+           )
+           Spacer(modifier = Modifier.width(8.dp))
+           Column(modifier = Modifier.weight(1f)) {
+               Text(
+                   text = "Hello $name!",
+                   fontSize = 24.sp,
+                   fontWeight = FontWeight.Bold,
+               )
+               Text(
+                   text = "Welcome to Dicoding",
+                   style = MaterialTheme.typography.body1.copy(
+                       fontStyle = FontStyle.Italic
+                   )
+               )
+           }
+           IconButton(onClick = { isExpended = !isExpended}) {
+               Icon(
+                   imageVector = if (isExpended) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
+                   contentDescription = if (isExpended) "Show less" else "Show more"
+               )
+           }
+       }
+   }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview(){
+    HelloJetpackComposeTheme() {
+        Greeting(name = "Jetpack Compose")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloJetpackComposeAppPreview(){
+    HelloJetpackComposeTheme {
+        HelloJetpackComposeApp()
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>){
+    if (names.isNotEmpty()) {
+       LazyColumn {
+           items(names) {name ->
+               Greeting(name)
+           }
+       }
+    }else{
+        Box(contentAlignment = Alignment.Center){
+            Text("No people to greet")
+        }
+    }
+}
